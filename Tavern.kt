2a5813b94754bc5a9bdfbb2ad9acf10808649990
generrr71 <54094050+generrr71@users.noreply.github.com>
2019-08-16 12:12:45 +0300

    Add files via upload
    Два разных вида вывода меню таверны по маске

--- Tavern.kt
+++ Tavern.kt
@@ -1,79 +1,148 @@
 import kotlin.math.roundToInt
-const val TAVERN_NAME = "Taernyl's Folly"
+import java.io.File
 
-var playerGold =10
-var playerSilver =10
+//const val TAVERN_NAME = "Taernyl's Folly"
+//
+//var playerGold =10
+//var playerSilver =10
+
+const val MENU_STRING_MAX_LENGTH = 35
+
+//val patronList = mutableListOf("Eli","Mordoc","Sophie")
+//val lastName = listOf("Ironfoot","Fernsworth","Baggins")
+
+//val uniquePatrons = mutableSetOf<String>()
+
+val menuList = File("data/tavern-menu-items.txt")
+    .readText()
+    .split("\n")
 
 fun main (args: Array<String>) {
 
-    placeOrder("shandy,Dragon's Breath,5.91")
-    // placeOrder("elixir,Shirley's Temple,4.12")
-    //   var beverage = readLine()
-//    var beverage = readLine()?.let {
-//        if(it.isNotBlank()){
-//            it.capitalize()
-//        }
-//        else {
-//            "Buttered Ale"
-//        }
-//    }
-//    beverage = null
-//    println(beverage)
+    var isFirstShandy = true
+    var isFirstElixir = true
+    var isFirstMeal =  true
+    var isFirstDessert = true
 
+    menuList.forEach{
+        inputStringFormatingMethod1(menuList.indexOf(it),it)
+    }
 
+    val sortedList =  menuList.sortedBy { it.first() }.reversed()
 
-}
+    println("")
 
-fun performPurchase(price:Double){
-    displayBalance()
-    val totalPurse = playerGold +(playerSilver/100.0)
-    println("Total purse: $totalPurse")
-    println("Purchasing item for $price")
+    println("*** Welcome to Taernyl's Folly ***")
+    sortedList.forEach()
+    {
+        if (it.split(",").first() == "shandy")
+        {
+            if (isFirstShandy) {
+                println("${" ".repeat((MENU_STRING_MAX_LENGTH/3).toInt())}" +"~[shandy]~")
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+                isFirstShandy = false
+            } else {
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+            }
 
-    val remainingBalance = totalPurse - price
-    println("Remaining balnce :${"%.2f".format(remainingBalance)}")
-
-    val remainingGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance%1*100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
-    displayBalance()
-}
-
-private fun displayBalance(){
-    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
-}
-
-private fun toDragonSpeak(phrase:String)=
-    phrase.replace(Regex("[aeiou]")) {
-        when (it.value) {
-            "a" -> "4"
-            "e" -> "3"
-            "i" -> "1"
-            "o" -> "0"
-            "u" -> "|_|"
-            else -> it.value
         }
+        else if (it.split(",").first() == "elixir")
+        {
+            if (isFirstElixir) {
+                println("${" ".repeat((MENU_STRING_MAX_LENGTH/3).toInt())}" +"~[elixir]~")
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+                isFirstElixir = false
+            } else {
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+            }
+
+        }
+        else if (it.split(",").first() == "meal")
+        {
+            if (isFirstMeal) {
+                println("${" ".repeat((MENU_STRING_MAX_LENGTH/3).toInt())}" +"~[meal]~")
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+                isFirstMeal = false
+            } else {
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+            }
+
+        }
+        else if (it.split(",").first() == "desert dessert")
+        {
+            if (isFirstDessert) {
+                println("${" ".repeat((MENU_STRING_MAX_LENGTH/3).toInt())}" + "~[desert dessert]~")
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+                isFirstDessert = false
+            } else {
+                inputStringFormatingMethod2(sortedList.indexOf(it), it)
+            }
+
+        }
+      //  println(it)
     }
 
-private fun placeOrder(menuData:String) {
-    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
-    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speaks with $tavernMaster about their order.")
 
-    val(type,name,price) = menuData.split(',')
-
-    val message = "Madrigal buys a $name ($type) for $price."
-    println(message)
-
-    performPurchase(price.toDouble())
-
-    val phrase = if (name == "Dragon's Breath"){
-        "Mardrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
-    } else {
-        "Mardigal says: Thanks for the $name"
-    }
-    println(phrase)
-//    val phrase = "Ah, delicious $name!"
-//    println("Madrigal ecxlaims: ${toDragonSpeak(phrase)}")
 }
+
+private fun inputStringFormatingMethod1 (index: Int, data: String){
+    val processingStringDataElements = data.split(",")
+    val itemType = processingStringDataElements[0]
+    val itemName = processingStringDataElements[1].capitalize()
+    val itemCost = processingStringDataElements[2]
+    val nextStringSpecialSymbolPresent = if (processingStringDataElements.last().contains('\r')){
+        1
+    } else {
+        0
+    }
+
+    var numberOfPointsToDraw = MENU_STRING_MAX_LENGTH - (itemName.length + itemCost.length -  nextStringSpecialSymbolPresent)
+    val pointsToDraw = "${".".repeat(numberOfPointsToDraw)}"
+
+    println("$itemName"+"$pointsToDraw"+"$itemCost" )
+}
+
+private fun inputStringFormatingMethod2(index: Int, data: String){
+
+
+
+    val processingStringDataElements = data.split(",")
+    val itemType = processingStringDataElements[0]
+    val itemName = processingStringDataElements[1].capitalize()
+    val itemCost = processingStringDataElements[2]
+    val nextStringSpecialSymbolPresent = if (processingStringDataElements.last().contains('\r')){
+        1
+    } else {
+        0
+    }
+
+    val desertShortedLine:Int = 6
+
+    val numberOfPointsToDraw:Int = if (itemType == "desert dessert") {
+              MENU_STRING_MAX_LENGTH - (itemName.length + itemCost.length - nextStringSpecialSymbolPresent) - desertShortedLine
+        } else {
+             MENU_STRING_MAX_LENGTH - (itemName.length + itemCost.length - nextStringSpecialSymbolPresent)
+        }
+
+    val pointsToDraw = "${".".repeat(numberOfPointsToDraw)}"
+
+
+    //println("shandy")
+  //  if(itemType=="shandy") {
+  //      println("$itemName"+"$pointsToDraw"+"$itemCost" )
+  //  }
+  //  else if(itemType=="elixir"){
+  //      println("$itemName"+"$pointsToDraw"+"$itemCost" )
+ //   }
+  //  else if (itemType=="meal"){
+  //      println("$itemName"+"$pointsToDraw"+"$itemCost" )
+  //  }
+  //  else if (itemType=="desert dessert"){
+
+        println("$itemName"+"$pointsToDraw"+"$itemCost" )
+   // } else {
+  //      println("")
+  //  }
+
+}
+
