39c8080aa0291095186bf6e1bcf103f04165a0e6
generrr71 <54094050+generrr71@users.noreply.github.com>
2019-08-16 16:30:00 +0300

    Add files via upload
    Реализация удаления посетителей с отрицательным балансом(ассоциативный массив)

--- Tavern.kt
+++ Tavern.kt
@@ -4,22 +4,24 @@ import java.io.File
 const val TAVERN_NAME = "Taernyl's Folly"
 
 
-val patronList = mutableListOf("Eli","Mordoc","Sophie")
-val lastName = listOf("Ironfoot","Fernsworth","Baggins")
+val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
 val uniquePatrons = mutableSetOf<String>()
 val menuList = File("data/tavern-menu-items.txt")
     .readText()
     .split("\n")
 
-val patronGold = mutableMapOf<String,Double>()
+val patronGold = mutableMapOf<String, Double>()
 
-fun main (args: Array<String>) {
-    if (patronList.contains("Eli")){
+var patronsToKickList = mutableSetOf<String>()
+
+fun main(args: Array<String>) {
+    if (patronList.contains("Eli")) {
         println("The tavern master says: Eli's in the back playing cards.")
     } else {
         println("Thr tavern master says: Eli isn't here")
     }
-    if (patronList.containsAll(listOf("Sophie","Mordoc"))){
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
         println("The tavern master says: Yea, they're seated by the stew kettle.")
     } else {
         println("The tavern master says: Nay, they departed hours ago.")
@@ -31,35 +33,57 @@ fun main (args: Array<String>) {
         val name = "$first $last"
         uniquePatrons += name
     }
-    uniquePatrons.forEach{
-        patronGold[it]=6.0
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
     }
 
     var orderCount = 0
-    while (orderCount <= 9 ){
-        placeOrder(uniquePatrons.shuffled().first(),
-            menuList.shuffled().first())
+    while (orderCount <= 9) {
+        placeOrder(
+            uniquePatrons.shuffled().first(),
+            menuList.shuffled().first()
+        )
         orderCount++
     }
 
     displaypatronBalance()
+
+    patronGold.forEach { (name, value) ->
+
+        if (value < 0) {
+            patronsToKickList.add(name)
+            //            patronGold.remove(name)
+//            println("$name removed for having $value")
+        } else {
+            //  println("$name")
+        }
+    }
+    println("Patrons to kick:")
+    println(patronsToKickList)
+    patronGold -= patronsToKickList
+    uniquePatrons -= patronsToKickList
+    println("Patrons remain:")
+    println(patronGold)
 }
 
-private fun displaypatronBalance(){
-    patronGold.forEach{ patron,balance ->
+//--------------------
+//-- FUNCTIONS --//
+
+private fun displaypatronBalance() {
+    patronGold.forEach { patron, balance ->
         println("$patron, balance : ${"%.2f".format(balance)}")
 
     }
 }
 
-private fun performPurchase(price:Double, patronName:String){
+private fun performPurchase(price: Double, patronName: String) {
     val totalPurse = patronGold.getValue(patronName)
     patronGold[patronName] = totalPurse - price
+
 }
 
 
-
-private fun toDragonSpeak(phrase:String)=
+private fun toDragonSpeak(phrase: String) =
     phrase.replace(Regex("[aeiou]")) {
         when (it.value) {
             "a" -> "4"
@@ -71,22 +95,25 @@ private fun toDragonSpeak(phrase:String)=
         }
     }
 
-private fun placeOrder(patronName: String, menuData:String) {
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("$patronName speaks with $tavernMaster about their order.")
 
-    val(type,name,price) = menuData.split(',')
+    val (type, name, price) = menuData.split(',')
 
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
 
-    performPurchase(price.toDouble(),patronName)
+    performPurchase(price.toDouble(), patronName)
 
-    val phrase = if (name == "Dragon's Breath"){
+
+    val phrase = if (name == "Dragon's Breath") {
         "$patronName exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
     } else {
         "$patronName says: Thanks for the $name"
     }
     println(phrase)
 }
+
+
