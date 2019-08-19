ccf0caadee2bad4dee76fe9b9f6bdc00c1b6839c
generrr71 <54094050+generrr71@users.noreply.github.com>
2019-08-19 11:35:15 +0300

    Add files via upload
    Конструкторы.
    Инициализация.

---
+++ Game.kt
@@ -0,0 +1,22 @@
+package com.bignerdranch.nyethack
+
+fun main(args: Array<String>) {
+
+    val player = Player("Madrigal")
+
+    player.castFireball()
+
+    //A
+    val auraColor = player.auraColor(player.isBlessed)
+
+    printPlayerStatus(player)
+    player.auraColor(player.isBlessed)
+}
+
+private fun  printPlayerStatus(player: Player){
+    println("(Aura: ${player.auraColor(player.isBlessed)})" +
+            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} ${player.formatHealthStatus(player.healthPoints,player.isBlessed)}")
+}
+
+
