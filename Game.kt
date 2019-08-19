a374775442bf355d8c0f69d8ef8e68cb0d10b060
generrr71 <54094050+generrr71@users.noreply.github.com>
2019-08-19 15:58:36 +0300

    Add files via upload
    Структуры данных. Объекты.Перечислене.
    Реализовано перемещение по "карте" через текстовые команды

--- Game.kt
+++ Game.kt
@@ -1,26 +1,73 @@
 package com.bignerdranch.nyethack
 
 fun main(args: Array<String>) {
+    //A
+    //   val auraColor = player.auraColor(player.isBlessed)
+    Game.play()
+    //  player.auraColor(player.isBlessed)
+}
+
+object Game {
 
     val player = Player("Madrigal")
+    var currentRoom: Room = TownSquare()
 
-    player.castFireball()
+    private var worldMap = listOf(
+        listOf(currentRoom, Room("Tavern"), Room("Back Room")),
+        listOf(Room("Long Corridor"), Room("Generic Room"))
+    )
 
-    var currentRoom:Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
+    init {
+        println("Welcome, advanturer.")
+        player.castFireball()
+    }
 
-    //A
-    val auraColor = player.auraColor(player.isBlessed)
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
+            printPlayerStatus(player)
 
-    printPlayerStatus(player)
-    player.auraColor(player.isBlessed)
+            print(">Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+            //plaing
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println(
+            "(Aura: ${player.auraColor(player.isBlessed)})" +
+                    "(Blessed: ${if (player.isBlessed) "YES" else "NO"})"
+        )
+        println("${player.name} ${player.formatHealthStatus(player.healthPoints, player.isBlessed)}")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1, { "" })
+
+        fun processCommand() = when (command.toLowerCase()) {
+            "move"-> move(argument)
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() = "I'm not quite sure what you're trying to do!"
+    }
+
+    private fun move(directionInput: String) = {
+        try {
+            val direction = Direction.valueOf(directionInput.toUpperCase())
+            val newPosition = direction.updateCoordinate(player.currentPosition)
+            if (!newPosition.isInBounds) {
+                throw IllegalStateException("$direction is out of bounds.")
+            }
+            val newRoom = worldMap[newPosition.y][newPosition.x]
+            player.currentPosition = newPosition
+            currentRoom = newRoom
+            "OK, you move $direction to the ${newRoom.name}.\n${newRoom.load()}"
+        } catch (e: Exception) {
+            "Invalid direction : $directionInput."
+        }
+    }
 }
-
-private fun  printPlayerStatus(player: Player){
-    println("(Aura: ${player.auraColor(player.isBlessed)})" +
-            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus(player.healthPoints,player.isBlessed)}")
-}
-
-
