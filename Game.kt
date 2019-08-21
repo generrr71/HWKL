7707339de93c325aedff76aa0c9fc9c34fa76e8f
generrr71 <54094050+generrr71@users.noreply.github.com>
2019-08-21 10:53:19 +0300

    Add files via upload
    Абстрактные классы. Реализация боя командой "fight"

--- Game.kt
+++ Game.kt
@@ -54,6 +54,7 @@ object Game {
         val argument = input.split(" ").getOrElse(1, { "" })
 
         fun processCommand() = when (command.toLowerCase()) {
+            "fight" -> fight()
             "move"-> move(argument)
             else -> commandNotFound()
         }
@@ -75,4 +76,26 @@ object Game {
         } catch (e: Exception) {
             "Invalid direction : $directionInput."
         }
+
+    private fun fight() = currentRoom.monster?.let {
+        while (player.healthPoints > 0 && it.healthPoints > 0 ){
+            slay(it)
+            Thread.sleep(1000)
+        }
+        "Combat complete"
+    } ?: "There's nothing here to fight"
+
+    private fun slay(monster: Monster){
+        println("${monster.name}) did ${monster.attack(player)} damage!")
+        println("${player.name} did ${player.attack(monster)} damage! ")
+
+        if (player.healthPoints <= 0) {
+            println (">>>> You have been defeated! Thanks for playing.<<<<")
+            exitProcess(0)
+        }
+        if (monster.healthPoints <=0 ){
+            println(">>>> ${monster.name} has been defeated! <<<<")
+            currentRoom.monster = null
+        }
+    }
 }
--- Room.kt
+++ Room.kt
@@ -2,8 +2,10 @@ package com.bignerdranch.nyethack
 
 open class Room (val name: String){
     protected open val dangerLevel = 5
+    var monster:Monster? = Goblin()
     fun description() = "Room: $name\n" +
-            "Danger level: $dangerLevel"
+            "Danger level: $dangerLevel\n"+
+            "Creature: ${monster?.description ?:"none."}"
 
     open fun load() = "Nothing much to see here..."
 }
@@ -11,6 +13,6 @@ open class Room (val name: String){
 open class TownSquare :Room("Town Square"){
     override val dangerLevel = super.dangerLevel -3
     private var bellSound = "GWONG"
-   final override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
+    final override fun load() = "The villagers rally and cheer as you enter!\n${ringBell()}"
     private fun ringBell()="The bell tower announces your arrival. $bellSound"
 }
